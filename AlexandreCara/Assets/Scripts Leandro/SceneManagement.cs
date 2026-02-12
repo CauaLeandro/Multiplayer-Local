7006f0352bffd85b04487e0183a5caddd252b09f
CauaLeandro <caualeandrocoelho0@gmail.com>
2026-02-12 09:38:15 -0300

    ola

---
+++ AlexandreCara/Assets/Scripts Leandro/SceneManagement.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDeath : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("morte"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
