8351cf42de51f01db39c66aa3f25cad1c17c23b5
CauaLeandro <caualeandrocoelho0@gmail.com>
2026-02-23 09:38:45 -0300

    Menu quase feito e level design 2ª fase

--- AlexandreCara/Assets/Scripts Leandro/SceneManagement.cs
+++ AlexandreCara/Assets/Scripts Leandro/SceneManagement.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PlayerDeath : MonoBehaviour
+public class SceneManagement : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("morte"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            
         }
 
         if (collision.gameObject.CompareTag("Portal"))
